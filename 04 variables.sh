8d5c598dec65777c9948333dd965810291c00f9e
harikrishna-nagamalla <harikrishna10465@gmail.com>
2025-09-28 15:38:06 +0530

    created 04 variables script

---
+++ 04 variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+$PERSON1=$1
+$PERSON2=$2
+
+echo "$PERSON1:: Welcome to Alaska"
+echo "$PERSON2 :: Thank you $PERSON1, Finally not on a zoom call"
+echo "$PERSON1 :: So, do we have a deal?"
+echo "$PERSON2 :: Yes, if you make Ukraine joins Russia"
