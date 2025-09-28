43cedd54164c34c73612c289c1fbc1a02a2f4505
harikrishna-nagamalla <harikrishna10465@gmail.com>
2025-09-28 14:12:56 +0530

    created 03 variables script

---
+++ 03 variables.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+PERSON1 = "Suresh"
+PERSON2 = "Ramesh"
+
+echo "$PERSON1 :: Hi $PERSON2, How are you?"
+echo "$PERSON2 :: Hello $PERSON1,  I am fine. How are you?"
+echo "$PERSON1 :: I am fine too. What are you learning now?"
+echo "$PERSON2 :: I am learning Shell Scripting $PERSON1."
