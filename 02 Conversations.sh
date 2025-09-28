31521f7da912436f0b2a857d2f1eea3e4e2b02f8
harikrishna-nagamalla <harikrishna10465@gmail.com>
2025-09-28 14:10:37 +0530

    created 02 conversations scrit

---
+++ 02 Conversations.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+echo "Suresh :: Hi Suresh, How are you?"
+echo "Ramesh :: Hello Ramesh,  I am fine. How are you?"
+echo "Suresh :: I am fine too. What are you learning now?"
+echo "Ramesh :: I am learning Shell Scripting Ramesh."
