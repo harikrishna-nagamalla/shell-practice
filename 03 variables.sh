2fcc054624a0edd932405367cd481d4cb05c1da1
harikrishna-nagamalla <harikrishna10465@gmail.com>
2025-09-28 14:15:27 +0530

    updated 03 variables script

--- 03 variables.sh
+++ 03 variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-PERSON1=Suresh
-PERSON2=Ramesh
+PERSON1=Shiva
+PERSON2=Ramu
 
 echo "$PERSON1 :: Hi $PERSON2, How are you?"
 echo "$PERSON2 :: Hello $PERSON1,  I am fine. How are you?"
