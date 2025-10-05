4208d70fc2886a39f2966e6155a8ae452fa6c92b
harikrishna-nagamalla <harikrishna10465@gmail.com>
2025-10-05 12:08:41 +0530

    Created Even Odd script

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -3,10 +3,29 @@
 echo "Enter the Number: "
 read NUMBER
 
-if [ $((NUMBER %2)) -eq 0 ]; then
-    echo "Given number $NUMBER is EVEN"
-else
-    echo "Given number $NUMBER is ODD"
+# if [ $((NUMBER %2)) -eq 0 ]; then
+#     echo "Given number $NUMBER is EVEN"
+# else
+#     echo "Given number $NUMBER is ODD"
+# fi
+
+# Checking whether the numer is Prime or not
+
+if [ $((NUMBER)) -eq 1 ]; then
+    echo "Given number $NUMBER is PRIME Number"
+if [ $((NUMBER)) -eq 2 ]; then
+    echo "Given number $NUMBER is PRIME Number"
+elif [ $((NUMBER)) -eq 3 ]; then
+    echo "Given number $NUMBER is PRIME Number"
+elif[ $((NUMBER)) -eq 5 ]; then
+    echo "Given number $NUMBER is PRIME Number"
+elif[ $((NUMBER)) -eq 7 ]; then
+    echo "Given number $NUMBER is PRIME Number"
+elif[ $((NUMBER)) -eq 11 ]; then
+    echo "Given number $NUMBER is PRIME Number"
 fi
 
+if [ $((NUMBER)) eq 2 ] || [ $((NUMBER)) eq 3 ] || [ $((NUMBER)) eq 5 ] || [ $((NUMBER)) eq 7 ] || [ $((NUMBER)) eq 11 ]; then
+    echo "Given number $NUMBER is PRIME Number" 
+fi
 
