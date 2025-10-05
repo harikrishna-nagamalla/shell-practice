c6c582cad465284ae212e3f5e73cc63f8aa24be7
harikrishna-nagamalla <harikrishna10465@gmail.com>
2025-10-05 08:46:36 +0530

    data tyes

--- 09-data-types.sh
+++ 09-data-types.sh
@@ -4,16 +4,16 @@
 
 NUMBER1=100
 NUMBER2=200
-NAME=DevOps
+# NAME=DevOps
 
-SUM=(($NUMBER1+$NUMBER2+$NAME))
+SUM=(($NUMBER1+$NUMBER2))
 
 echo "Sum is : {$SUM}" # here, shell understood & performed arithmetic operation only and excluded $NAME. considered as 0
 
-# size 4, max. index 3
-LEADERS=("Modi" "Trump" "Trudo" "Putin")
+# # size 4, max. index 3
+# LEADERS=("Modi" "Trump" "Trudo" "Putin")
 
 
-echo "All Leadres : ${LEADERS[@]}"  # All leaders in an array
-echo "First Leader is : ${LEADERS[0]}" # First leader
-echo "First Leader is : ${LEADERS[10]}" # Trying to ask no leader present here. Hence output will be empty
+# echo "All Leadres : ${LEADERS[@]}"  # All leaders in an array
+# echo "First Leader is : ${LEADERS[0]}" # First leader
+# echo "First Leader is : ${LEADERS[10]}" # Trying to ask no leader present here. Hence output will be empty
