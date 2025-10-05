17489e30fa2e255d0a00b2a48d7c85e5001f3763
harikrishna-nagamalla <harikrishna10465@gmail.com>
2025-10-05 08:44:50 +0530

    initial changes on 09 script

--- 09-data-types.sh
+++ 09-data-types.sh
@@ -6,7 +6,7 @@ NUMBER1=100
 NUMBER2=200
 NAME=DevOps
 
-SUM=(($NUMBER1 + $NUMBER2 + $NAME))
+SUM=(($NUMBER1+$NUMBER2+$NAME))
 
 echo "Sum is : {$SUM}" # here, shell understood & performed arithmetic operation only and excluded $NAME. considered as 0
 
