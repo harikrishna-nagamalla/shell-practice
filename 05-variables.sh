ad8e34b67760f25a15b58e7a33f38ef123ea4863
harikrishna-nagamalla <harikrishna10465@gmail.com>
2025-10-05 07:51:13 +0530

    updated 05 variables script

--- 05-variables.sh
+++ 05-variables.sh
@@ -3,4 +3,5 @@
 echo "Enter your mobile number :"
 read -s MOBILE_NUMBER
 
-echo "Your mobile number is : $MOBILE_NUMBER"
+# echo "Your mobile number is : $MOBILE_NUMBER"
+echo "Registerd Your Mobiile Number"
