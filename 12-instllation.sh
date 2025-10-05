17a65ef2146a237100acf2457ba0797704e49ceb
harikrishna-nagamalla <harikrishna10465@gmail.com>
2025-10-05 12:38:42 +0530

    Updated the installation script

--- 12-instllation.sh
+++ 12-instllation.sh
@@ -6,6 +6,10 @@ if [ $USERID -ne 0 ]; then
     echo "ERROR :: Please Run this scrpt with root privileges"
 fi
 
+dnf install mysql -y
+
 if [ $? -ne 0 ]; then
-    echo "ERROR"
+    echo "ERROR :: Installing MYSQL is FAILURE"
+else
+    echo "Installing MYSQL is SUCCESS"
 fi
