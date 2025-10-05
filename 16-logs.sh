b8a74f35d644a32d60cde333da150531c1dca506
harikrishna-nagamalla <harikrishna10465@gmail.com>
2025-10-05 15:02:14 +0530

    Created script for functions

--- 16-logs.sh
+++ 16-logs.sh
@@ -7,11 +7,12 @@ G="\e[32m"
 Y="\e[33m"
 N="\e[0m"
 
-LOGS_FOLDER = "/var/log/shell-script"
-SCRIPT_NAME = $( echo $0 | cut -d "." -f1 )
-LOG_FILE = "$LOGS_FOLDER/$SCRIPT_NAME.log" 
+LOGS_FOLDER="/var/log/shell-script"
+SCRIPT_NAME=$( echo $0 | cut -d "." -f1 )
+LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log" 
 
 mkdir -p $LOGS_FOLDER
+echo "Script started executed at: $(date)" | tee -a $LOG_FILE
 
 if [ $USERID -ne 0 ]; then
     echo "ERROR :: Run this script with root previleges"
