8bfcacc67669d89c4e600fe5e0626b94ceb18643
harikrishna-nagamalla <harikrishna10465@gmail.com>
2025-10-05 12:07:45 +0530

    Created Even Odd script

--- 11-conditions.sh
+++ 11-conditions.sh
@@ -3,7 +3,10 @@
 echo "Enter the Number: "
 read NUMBER
 
-if [ $((NUMBER)) eq 2 ] || [ $((NUMBER)) eq 3 ] || [ $((NUMBER)) eq 5 ] || [ $((NUMBER)) eq 7 ] || [ $((NUMBER)) eq 11 ]; then
-    echo "Given number $NUMBER is PRIME Number" 
+if [ $((NUMBER %2)) -eq 0 ]; then
+    echo "Given number $NUMBER is EVEN"
+else
+    echo "Given number $NUMBER is ODD"
 fi
+
 
